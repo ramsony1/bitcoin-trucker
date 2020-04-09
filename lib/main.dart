022103baf7c3e3fae0baa3e7ffd5d6788faaafc4
ramsony1 <62914223+ramsony1@users.noreply.github.com>
2020-04-09 11:56:57 -0400

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,18 @@
+import 'package:bitcointrucker/price_sreen.dart';
+import 'package:flutter/material.dart';
+
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData.dark().copyWith(
+          primaryColor: Colors.lightBlue,
+          scaffoldBackgroundColor: Colors.white),
+      home: PriceScreen(),
+    );
+  }
+}
